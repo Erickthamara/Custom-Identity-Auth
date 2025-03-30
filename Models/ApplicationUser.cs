ed71b0ccf2fe4bc38fce6f7e06db74571834b140
Erick Thamara <erickthamara9@gmail.com>
2025-03-30 21:21:07 +0300

    Add project files.

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Custom_Identity_Auth.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string IdNo { get; set; }
+    }
+}
